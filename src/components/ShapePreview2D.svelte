dc09700ce524e8003566c3b72c57e307103c3287
Melody Horn <melody@boringcactus.com>
2020-07-20 09:22:26 -0600

    improve svg zoom
    applies to #2 but there are still bugs to fix

--- src/components/ShapePreview2D.svelte
+++ src/components/ShapePreview2D.svelte
@@ -22,15 +22,17 @@
     let walls;
     $: walls = shape.calcWalls();
 
-    function px2svg(pxLen, units) {
+    function px2svg(pxLen, units, zoom) {
         // on my current primary monitor, 72pt is 96px
         let ptLen = pxLen * 72 / 96;
         // these numbers are from https://www.w3.org/Style/Examples/007/units.en.html
+        let svgLen;
         if (units === 'in') {
-            return ptLen / 72;
+            svgLen = ptLen / 72;
         } else if (units === 'cm') {
-            return ptLen / 72 * 2.54;
+            svgLen = ptLen / 72 * 2.54;
         }
+        return svgLen / zoom;
     }
 
     function clamp(val, min, max = undefined) {
@@ -59,8 +61,8 @@
     let vbHeight;
 
     $: {
-        vbWidth = px2svg(svgWidth, shape.units);
-        vbHeight = px2svg(svgHeight, shape.units);
+        vbWidth = px2svg(svgWidth, shape.units, zoom);
+        vbHeight = px2svg(svgHeight, shape.units, zoom);
         centerX = clamp(centerX, shape.calcPDFWidth() / 2);
         centerY = clamp(centerY, shape.calcPDFWidth() / 2);
     }
@@ -77,8 +79,8 @@
 
     function handleMouseMove(event) {
         if (dragging) {
-            centerX = clamp(centerX - px2svg(event.offsetX - dragLastX, shape.units), shape.calcPDFWidth() / 2);
-            centerY = clamp(centerY - px2svg(event.offsetY - dragLastY, shape.units), shape.calcPDFWidth() / 2);
+            centerX = clamp(centerX - px2svg(event.offsetX - dragLastX, shape.units, zoom), shape.calcPDFWidth() / 2);
+            centerY = clamp(centerY - px2svg(event.offsetY - dragLastY, shape.units, zoom), shape.calcPDFWidth() / 2);
             dragLastX = event.offsetX;
             dragLastY = event.offsetY;
         }
@@ -89,16 +91,23 @@
     }
 
     function handleScroll(event) {
+        // we want the place where the mouse was to remain fixed
+        let oldSvgX = px2svg(event.offsetX - svgWidth / 2, shape.units, zoom);
+        let oldSvgY = px2svg(event.offsetY - svgHeight / 2, shape.units, zoom);
         if (event.deltaY > 0) {
             zoom /= 1.2;
         } else {
             zoom *= 1.2;
         }
+        let newSvgX = px2svg(event.offsetX - svgWidth / 2, shape.units, zoom);
+        let newSvgY = px2svg(event.offsetY - svgHeight / 2, shape.units, zoom);
+        centerX -= (newSvgX - oldSvgX);
+        centerY -= (newSvgY - oldSvgY);
     }
 </script>
 
 <svg bind:this={svg}
-     viewBox="{centerX - (vbWidth / zoom) / 2} {centerY - (vbHeight / zoom) / 2} {vbWidth / zoom} {vbHeight / zoom}"
+     viewBox="{centerX - vbWidth / 2} {centerY - vbHeight / 2} {vbWidth} {vbHeight}"
      on:mousedown={handleMouseDown}
      on:mousemove={handleMouseMove}
      on:mouseup={handleMouseUp}
