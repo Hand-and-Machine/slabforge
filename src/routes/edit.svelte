6e0f2f64e8711245d199eb36288982380628244d
Melody Horn <melody@boringcactus.com>
2020-07-23 09:54:36 -0600

    add custom sides
    fixes #18

--- src/routes/edit.svelte
+++ src/routes/edit.svelte
@@ -22,11 +22,18 @@
 	import ShapePreview3D from '../components/ShapePreview3D.svelte';
 	import RadioSelector from '../components/RadioSelector.svelte';
 
-	let sides = 4;
+	let sidesSelection = 4;
+	let sides = sidesSelection;
 	let height = 5;
 	let bottomWidth = 5;
 	let topWidth = 5;
 	let units = 'cm';
+
+	$: {
+		if (sidesSelection !== 'custom') {
+			sides = sidesSelection;
+		}
+	}
 
 	let shape;
 	$: shape = makeShape(sides, height, bottomWidth, topWidth, units);
@@ -40,7 +47,10 @@
 <ShapePreview2D shape={shape}/>
 <ShapePreview3D shape={shape}/>
 <aside>
-	<RadioSelector bind:value={sides} options={[[3, '3 sides'], [4, '4 sides'], [6, '6 sides'], [8, '8 sides'], [10, '10 sides'], ['∞', 'circle']]}/>
+	<RadioSelector bind:value={sidesSelection} options={[[3, '3 sides'], [4, '4 sides'], [6, '6 sides'], ['custom', 'custom sides'], ['∞', 'circle']]}/>
+	{#if sidesSelection === 'custom'}
+		<SpinnerSliderControl bind:value={sides} min="3" step="1" max="20">Sides</SpinnerSliderControl>
+	{/if}
 	<SpinnerSliderControl bind:value={height} min="1" step="0.1">Height</SpinnerSliderControl>
 	<SpinnerSliderControl bind:value={bottomWidth} min="1" step="0.1">Bottom Width</SpinnerSliderControl>
 	<SpinnerSliderControl bind:value={topWidth} min="1" step="0.1">Top Width</SpinnerSliderControl>
